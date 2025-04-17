3482225b2d48153a1eb83b5e69e9f9e2c2531337
Rostislav Kupriyanov <83307529+njiaaah@users.noreply.github.com>
2025-04-17 18:47:56 +0300

    style: apply some default lib styling

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 export const metadata: Metadata = {
 	title: "Tracker app",
