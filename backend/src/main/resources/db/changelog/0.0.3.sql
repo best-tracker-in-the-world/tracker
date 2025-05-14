d22cfeb1b229a2cce677e078cadffa48437edcf3
rogzy <rogzy1606@gmail.com>
2025-05-14 16:31:18 +0300

    add test user (not-production)

---
+++ backend/src/main/resources/db/changelog/0.0.3.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (`id`, `created_at`, `updated_at`, `login`, `email`, `password_hash`, `verified`)
+VALUES (NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL, 'test@test.ru', '123456', '1');
