5e31a7705fcfc2ed37e2a4da181469234db1099e
njiah <njiah@njiah.ru>
2025-05-19 19:05:56 +0300

    basic pwa setup

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,41 +1,63 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-								devtools: { enabled: true },
+	devtools: { enabled: true },
 
-								modules: [
-								  "@nuxt/ui",
-								  "@nuxt/eslint",
-								  "@nuxt/fonts",
-								  "@nuxt/icon",
-								  "@nuxt/image",
-								  "@nuxt/scripts",
-								  "@nuxt/test-utils",
-								  "@pinia/nuxt",
-								  "@nuxtjs/i18n",
-								  "@formkit/auto-animate",
-								  "@vueuse/nuxt",
-								],
+	modules: [
+		"@nuxt/ui",
+		"@nuxt/eslint",
+		"@nuxt/fonts",
+		"@nuxt/icon",
+		"@nuxt/image",
+		"@nuxt/scripts",
+		"@nuxt/test-utils",
+		"@pinia/nuxt",
+		"@nuxtjs/i18n",
+		"@formkit/auto-animate",
+		"@vueuse/nuxt",
+		"@vite-pwa/nuxt",
+	],
+	pwa: {
+		manifest: {
+			name: "Tracker App",
+			short_name: "Tracker App",
+			description: "testing description cock ass",
+			icons: [
+				{
+					src: "icons/192.png",
+					sizes: "192x192",
+					type: "image/png",
+				},
+			],
+		},
+		workbox: {
+			navigateFallback: "/",
+		},
+		devOptions: {
+			enabled: true,
+			type: "module",
+		},
+	},
 
-								css: ["~/assets/css/main.css"],
+	css: ["~/assets/css/main.css"],
 
-								future: {
-																compatibilityVersion: 4,
-								},
-								i18n: {
-																defaultLocale: "ru",
-																locales: [
-																								{ code: "en", name: "English", file: "en.json" },
-																								{ code: "ru", name: "Русский", file: "ru.json" },
-																],
-																lazy: true,
-																langDir: "locales/",
-								},
+	future: {
+		compatibilityVersion: 4,
+	},
+	i18n: {
+		defaultLocale: "ru",
+		locales: [
+			{ code: "en", name: "English", file: "en.json" },
+			{ code: "ru", name: "Русский", file: "ru.json" },
+		],
+		lazy: true,
+		langDir: "locales/",
+	},
 
-								compatibilityDate: "2024-11-27",
+	compatibilityDate: "2024-11-27",
 
-								runtimeConfig: {
-																public: {
-																								NUXT_PUBLIC_API_URL: process.env.NUXT_PUBLIC_API_URL,
-																},
-								},
+	runtimeConfig: {
+		public: {
+			NUXT_PUBLIC_API_URL: process.env.NUXT_PUBLIC_API_URL,
+		},
+	},
 });
