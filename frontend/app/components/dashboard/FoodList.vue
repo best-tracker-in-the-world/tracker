69d404d0e2c403d583abd45322cc5899b9c4d252
njiaaah <njiah@njiah.ru>
2025-07-18 15:00:28 +0300

    cock

--- frontend/app/components/dashboard/FoodList.vue
+++ frontend/app/components/dashboard/FoodList.vue
@@ -36,7 +36,7 @@
 				>
 					<!-- default view -->
 					<div
-						class="food-item-content | items-center px-4 gap-2"
+						class="food-item-content | items-center gap-2"
 					>
 						<div
 							class="overflow-clip max-w-[50vw] flex flex-col"
--- frontend/app/pages/stats/index.vue
+++ frontend/app/pages/stats/index.vue
@@ -5,7 +5,7 @@
 				<UTabs v-model="currentTab" :items="tabs" />
 			</div>
 			<div class="w-full h-[400px] min-w-0">
-				<VChart :option="option" autoresize class="w-full h-full" />
+				<VChart :option="options" autoresize class="w-full h-full" />
 			</div>
 		</ClientOnly>
 	</div>
@@ -16,6 +16,9 @@ import "echarts";
 import type { RadioGroupItem } from "@nuxt/ui";
 
 const { t } = useI18n();
+const userSettings = useSettingsStore();
+
+console.log('settings', userSettings)
 
 interface TabItems {
 	label: string;
@@ -39,50 +42,16 @@ const tabs = ref<TabItems[]>([
 	},
 ]);
 
-const generateDateRange = (endDateString: string, days: number) => {
-	const endDate = new Date(endDateString);
-	const dates: string[] = [];
-	const labels: string[] = [];
-	const weights: number[] = [];
-	const calories: number[] = [];
-	const shortDays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-	
-	for (let i = days - 1; i >= 0; i--) {
-		const currentDate = new Date(endDate);
-		currentDate.setDate(endDate.getDate() - i);
-		const dateString = currentDate.toISOString().split('T')[0];
-		
-		dates.push(dateString);
-		labels.push(t(`calendar.${shortDays[currentDate.getDay()]}`));
-		
-		const dayData = dashboard.days.find(x => x.value.date === dateString);
-		weights.push(dayData?.value.weight || 0);
-		
-		const totalCalories = dayData?.value.foodLogs.reduce((sum, log) => sum + (log.caloricContent || 0), 0) || 0;
-		calories.push(totalCalories);
-	}
-	
-	return { dates, labels, weights, calories };
-};
+const shortDays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const weightsData = ref<number[]>();
 
-const updateChartData = () => {
-	const today = new Date().toISOString().split('T')[0];
-	const periodDays = { week: 7, month: 30, year: 365 };
-	const days = periodDays[currentTab.value] || 7;
-	
-	const { dates, labels, weights, calories } = generateDateRange(today, days);
-	
-	option.value.xAxis.data = labels;
-	option.value.series[0].data = weights;
-	// option.value.series[1].data = new Array(weights.length).fill(2000); 
-};
 
-const option = ref<ECOption>({
+const options = ref({
 	width: "80%",
 	height: "auto",
 	xAxis: {
 		type: "category",
-		data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+		data: shortDays.map(day => t(`calendar.${day}`)	),
 	},
 	yAxis: {},
 	series: [
@@ -90,7 +59,7 @@ const option = ref<ECOption>({
 			name: "weight",
 			type: "line",
 			symbol: "none",
-			data: [100, 94, 99, 95, 90, 91, 89.1],
+			data: weightsData.value,
 		},
 		{
 			name: "weight goal",
@@ -105,6 +74,42 @@ const option = ref<ECOption>({
 	],
 });
 
+const generateDateRange = (endDateString: string, days: number) => {
+	const endDate = new Date(endDateString);
+	const dates: string[] = [];
+	const labels: string[] = [];
+	const weights: number[] = [];
+	const calories: number[] = [];
+
+	
+	for (let i = days - 1; i >= 0; i--) {
+		const currentDate = new Date(endDate);
+		currentDate.setDate(endDate.getDate() - i);
+		const dateString = currentDate.toISOString().split('T')[0];
+		
+		dates.push(dateString);
+		labels.push(t(`calendar.${shortDays[currentDate.getDay()]}`));
+		
+		const dayData = Array.from(dashboard.days.entries()).find(([key, value]) => value.date === dateString);
+		weights.push(dayData?.[1].weight ?? 0)
+
+
+	}
+	return { dates, labels, weights, calories };
+};
+
+const updateChartData = () => {
+	const today = new Date().toISOString().split('T')[0];
+	const periodDays = { week: 7, month: 30, year: 365 };
+	const days = periodDays[currentTab.value as keyof typeof periodDays] || 7;
+	
+	const { weights } = generateDateRange(today!, days);
+	weightsData.value = weights;
+	console.log(options.value.series[0])
+	console.log(options.value.series[0])
+};
+
+
 const isMobile = useIsMobile();
 const dashboard = useDashboardStore();
 
@@ -112,8 +117,7 @@ watch(() => currentTab.value, updateChartData);
 
 onMounted(async () => {
 	await dashboard.loadAllDays();
-	console.log(dashboard.days);
-	// updateChartData();
+	updateChartData();
 });
 
 definePageMeta({
